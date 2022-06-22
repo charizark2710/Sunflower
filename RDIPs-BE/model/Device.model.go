27b0da9ef9883850898f16e5a25a40c6e8658d0f
charizark2710 <dangduchieudn99@gmail.com>
2022-06-23 01:14:28 +0700

    remove comment lines

--- RDIPs-BE/model/Device.model.go
+++ RDIPs-BE/model/Device.model.go
@@ -9,20 +9,3 @@ type SysDevice struct {
 	Id   string `gorm:"default:uuid_generate_v4();primaryKey;column:id;type:uuid"`
 	Name string `gorm:"column:name"`
 }
-
-// func (SysDevice) TableName() string {
-// 	return "sys_devices"
-// }
-
-// func (m *SysDevice) CreateDevices() error {
-// 	db := DbHelper.GetDb()
-// 	if !db.Migrator().HasTable(m) {
-// 		err := db.Migrator().CreateTable(m)
-// 		if err != nil {
-// 			return err
-// 		}
-// 		err = db.Save(m).Error
-// 		return err
-// 	}
-// 	return db.Save(m).Error
-// }
