7bb1d8c72fd7f88ad0a284aba442ea08074baca9
charizark2710 <dangduchieudn99@gmail.com>
2022-07-10 17:08:01 +0700

    add rabbitmq config file

--- RDIPs-BE/handler/Sys.handler.go
+++ RDIPs-BE/handler/Sys.handler.go
@@ -4,44 +4,45 @@ import (
 	"reflect"
 
 	"github.com/charizark2710/Sunflower/RDIPs-BE/model"
+	"gorm.io/gorm"
 )
 
-func getStructType(i interface{}) interface{} {
+func getStructType(i interface{}, db *gorm.DB) interface{} {
 	reflectModel := reflect.ValueOf(i)
-	return reflectModel.Interface()
-}
-
-func Read(i interface{}) error {
-	db := model.DbHelper.GetDb()
-	model := getStructType(i)
-	err := db.Find(model).Where("deleted = ?", true).Error
-	return err
-}
-
-func Create(i interface{}) error {
-	db := model.DbHelper.GetDb()
-	m := getStructType(i)
+	m := reflectModel.Interface()
 	if !db.Migrator().HasTable(m) {
 		err := db.Migrator().CreateTable(m)
 		if err != nil {
 			return err
 		}
-		err = db.Save(m).Error
 		return err
 	}
+	return m
+}
+
+func Read(i interface{}) error {
+	db := model.DbHelper.GetDb()
+	model := getStructType(i, db)
+	err := db.Find(model).Where("deleted = ?", false).Error
+	return err
+}
+
+func Create(i interface{}) error {
+	db := model.DbHelper.GetDb()
+	m := getStructType(i, db)
 	return db.Save(m).Error
 }
 
 func ReadDetail(i interface{}, id string) error {
 	db := model.DbHelper.GetDb()
-	model := getStructType(i)
-	err := db.Where("id = ? AND deleted = ?", id, true).First(model).Error
+	model := getStructType(i, db)
+	err := db.Where("id = ? AND deleted = ?", id, false).First(model).Error
 	return err
 }
 
 func Update(i interface{}, updatedData interface{}) error {
 	db := model.DbHelper.GetDb()
-	model := getStructType(i)
+	model := getStructType(i, db)
 	err := db.Model(model).Updates(updatedData).Error
 	return err
 }
