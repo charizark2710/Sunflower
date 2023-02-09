f8e233ab44064117b06620933c861023660ddb6f
Error Segment <dangduchieudn99@gmail.com>
2023-02-09 00:17:51 +0000

    refactor code

--- RDIPs-BE/model/common/Credential.model.go
+++ RDIPs-BE/model/common/Credential.model.go
@@ -1,6 +1,8 @@
 package model
 
 import (
+	"fmt"
+
 	"github.com/google/uuid"
 )
 
@@ -8,4 +10,9 @@ type Credential struct {
 	UserName  string      `json:"userName"`
 	OtherInfo interface{} `json:"otherInfo"`
 	ID        uuid.UUID   `json:"id"`
+}
+
+func (credential *Credential) Valid() error {
+	fmt.Print(credential)
+	return nil
 }
