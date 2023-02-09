fe3fc8cafa5c8b0ae93c0bac1b69d37ff2a2ce1b
Error Segment <dangduchieudn99@gmail.com>
2023-02-09 00:17:51 +0000

    refactor code

--- RDIPs-BE/handler/Sys.handler.go
+++ RDIPs-BE/handler/Sys.handler.go
@@ -3,9 +3,11 @@ package handler
 import (
 	"reflect"
 
-	"github.com/charizark2710/Sunflower/RDIPs-BE/model"
+	commonModel "github.com/charizark2710/Sunflower/RDIPs-BE/model/common"
 	"gorm.io/gorm"
 )
+
+var db = commonModel.DbHelper.GetDb()
 
 func getStructType(i interface{}, db *gorm.DB) interface{} {
 	reflectModel := reflect.ValueOf(i)
@@ -21,27 +23,23 @@ func getStructType(i interface{}, db *gorm.DB) interface{} {
 }
 
 func Read(i interface{}) error {
-	db := model.DbHelper.GetDb()
 	model := getStructType(i, db)
 	err := db.Find(model).Where("deleted = ?", false).Error
 	return err
 }
 
 func Create(i interface{}) error {
-	db := model.DbHelper.GetDb()
 	m := getStructType(i, db)
 	return db.Save(m).Error
 }
 
 func ReadDetail(i interface{}, id string) error {
-	db := model.DbHelper.GetDb()
 	model := getStructType(i, db)
 	err := db.Where("id = ? AND deleted = ?", id, false).First(model).Error
 	return err
 }
 
 func Update(i interface{}, updatedData interface{}) error {
-	db := model.DbHelper.GetDb()
 	model := getStructType(i, db)
 	err := db.Model(model).Updates(updatedData).Error
 	return err
