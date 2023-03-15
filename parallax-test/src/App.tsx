173ef74194ac44f7fc3aebfc49de1d597c911c10
thanh85tttt <thanh85ttttt@gmail.com>
2023-03-15 22:25:35 +0700

    feat: first page scroll

--- parallax-test/src/App.tsx
+++ parallax-test/src/App.tsx
@@ -1,13 +1,13 @@
 import './App.css';
 import React from 'react';
 import { ParallaxProvider } from 'react-scroll-parallax';
-import BannerOne from './component/bannerone/BannerOne';
+import BannerZero from './component/bannerzero/BannerZero';
 
 function App() {
   return (
     <div id='parallax'>
       <ParallaxProvider>
-        <BannerOne />
+        <BannerZero />
       </ParallaxProvider>
     </div>
   );
---
+++ parallax-test/src/component/bannerzero/BannerZero.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./BannerZero.css";
+import KeyboardDoubleArrowDownIcon from "@mui/icons-material/KeyboardDoubleArrowDown";
+
+const BannerZero = () => {
+  return (
+    <div className="banner-zero">
+      <div className="container">
+        <p className="scroll-text">Scroll Down</p>
+        <div className="arrow-down-container">
+          <KeyboardDoubleArrowDownIcon className="arrow-down" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default BannerZero;
