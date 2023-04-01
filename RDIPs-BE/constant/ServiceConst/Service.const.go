086bab53371f84b4f730d2e4825858540379b9f7
thanh85tttt <thanh85ttttt@gmail.com>
2023-04-01 14:52:10 +0700

    feat: Add performance model

--- RDIPs-BE/constant/ServiceConst/Service.const.go
+++ RDIPs-BE/constant/ServiceConst/Service.const.go
@@ -11,4 +11,11 @@ var ServicesMap = map[string]interface{}{
 	"GET" + urlconst.GetDetailDevice: services.GetDetailDevice,
 	"PUT" + urlconst.PutDetailDevice: services.UpdateDevice,
 	"DELETE" + urlconst.DeleteDevice: services.DeleteDevice,
+
+	//Performances
+	"GET" + urlconst.GetAllPerformances:   services.GetAllPerformances,
+	"POST" + urlconst.PostPerformance:     services.PostPerformance,
+	"GET" + urlconst.GetDetailPerformance: services.GetDetailPerformance,
+	"PUT" + urlconst.PutDetailPerformance: services.PutPerformance,
+	"DELETE" + urlconst.DeletePerformance: services.DeletePerformance,
 }
