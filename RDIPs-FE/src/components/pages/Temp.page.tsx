65144a31da50ef5376302c93d4b7520858e40606
Truc Ly Nguyen <truclyy38@gmail.com>
2023-04-17 22:53:29 +0700

    merge highchart into code

--- RDIPs-FE/src/components/pages/Temp.page.tsx
+++ RDIPs-FE/src/components/pages/Temp.page.tsx
@@ -1,8 +1,86 @@
+import { Box, FormControl, InputLabel, MenuItem, Select, SelectChangeEvent } from '@mui/material';
+import React from 'react';
+import chartData from '../../lib/chartData.json';
+import DatePickerCustom from '../../lib/highchart/DatePickerCustom';
+import { HighChartCustom } from '../../lib/highchart/HighChartCustom';
+import { TypeChart } from '../../utils/enum';
 import './Temp.page.scss';
+
 function TempPage() {
-    return (
-        <div className="Sidebar" > Thấy thì mất 5 nghìn</div>
-    )
+   const [type, setType] = React.useState('1');
+
+  const handleChange = (event: SelectChangeEvent) => {
+    setType(event.target.value);
+  };
+
+  const onNextClick = () => {
+    console.log("Xứng đáng là con trai của taaaaa next");
+    
+  }
+
+  const onPrevClick = () => {
+    console.log("Xứng đáng là con trai của taaaaa right");
+    
+  }
+
+  const listTimeType = ["Day", "Week", "Month", "Year", "Decade"];
+
+  React.useEffect(()=>{
+    console.log(type);
+    
+  }, [type])
+
+
+  return (
+    // <div className="Sidebar" > Thấy thì mất 5 nghìn</div>
+
+    <div style={{padding: '15px'}}>
+      <h5>Range Selection</h5>
+
+      <Box
+        sx={{
+          display: 'grid',
+          columnGap: 3,
+          rowGap: 1,
+          gridTemplateColumns: 'repeat(2, 1fr)',
+        }}
+      >
+        <span>
+        <FormControl fullWidth>
+            <InputLabel id="demo-simple-select-label">Filter By</InputLabel>
+            <Select
+                labelId="demo-simple-select-label"
+                value={type}
+                id='dateSelect' variant='outlined' sx={{ m: 2, maxWidth: 300 }} size='small'
+                onChange={handleChange}
+            >
+                {listTimeType.map((type,i)=>{
+                   return <MenuItem key={i} value={i+1}>{type}</MenuItem>
+                })}
+            </Select>
+        </FormControl>
+        </span>
+      </Box>
+      <Box>
+        <DatePickerCustom fromDate={chartData[0].dateTime} onNextClick={onNextClick} onPrevClick={onPrevClick} disabled={false}></DatePickerCustom>
+      </Box>
+
+      <h5>Case 1: Sline chart </h5>
+      <HighChartCustom
+        typeChart={TypeChart.sline}
+        timeType={+type}
+        chartData={chartData as any}
+        titleChart={'Daily Power Chart'}
+      ></HighChartCustom>
+      <h5>Case 2: Bar chart </h5>
+      <HighChartCustom
+        typeChart={TypeChart.bar}
+        timeType={+type}
+        chartData={chartData as any}
+        titleChart={'Daily Power Chart'}
+      ></HighChartCustom>
+    </div>
+  );
 }
 
-export default TempPage
+export default TempPage;
---
+++ RDIPs-FE/src/lib/highchart/HighChartCustom.tsx
@@ -0,0 +1,217 @@
+import { HighchartsReact } from 'highcharts-react-official';
+import Highcharts from 'highcharts/highstock';
+import React, { useEffect, useMemo } from 'react';
+import { DateFilter, TypeChart } from '../../utils/enum';
+
+export interface HighChartsProps {
+  timeType: DateFilter;
+  typeChart: TypeChart;
+  chartData: DataDetail[];
+  titleChart: string | null;
+}
+
+export interface DataDetail {
+  AO: number;
+  AC: number;
+  AL: number;
+  dateTime: string;
+  unit: string;
+}
+
+export const HighChartCustom: React.FC<HighChartsProps> = (props : HighChartsProps) => {
+  const [currentData, setCurrentData] = React.useState(props.chartData);
+  let currentSelectedFromDate = null;//choose from the date min in timeline
+  let currentSelectedToDate = null;//choose from the date max in timeline
+  let amountDisplay :number = 0;// distance between them
+  useMemo(()=>{
+
+    switch(props.timeType) {
+      case DateFilter.Hour: {
+        console.log("in 1", props.chartData);
+        amountDisplay = 24
+        break;
+      }
+      case DateFilter.Date: {
+        console.log("in 2");
+
+        break;
+      }
+      case DateFilter.Week: {
+        console.log("in 3");
+
+        break;
+      }
+      case DateFilter.Month: {
+        console.log("in 4");
+
+        break;
+      }
+      case DateFilter.Year: {
+        console.log("in 5");
+
+        break;
+      }
+    }
+  }, [props])
+
+  function fromDate() {
+    return null;
+  }
+
+  function toDate() {
+    return null;
+  }
+
+  function getOptions(typeChart: TypeChart) {
+    switch(typeChart) {
+      case 1: {
+        return optionsSplineChart;
+      }
+      case 2: {
+        return optionsBarChart;
+      }
+    }
+  }
+  const monthData = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+  const hourData = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24]
+  
+  function filterDataBy(type : DateFilter, data: number) {
+
+    return null;
+  }
+
+  const optionsSplineChart = {
+    chart: {
+      type: 'spline'
+    },
+    title: {
+      text: props.titleChart
+    },
+    xAxis: {
+      categories: hourData,
+      title: {
+        text: `Time (${DateFilter[props.timeType]})`
+      }
+    },
+    yAxis: {
+      title: {
+        text: `A (${props.chartData[0].unit})`
+      }
+    },
+    plotOptions: {
+      line: {
+        dataLabels: {
+          enabled: true
+        },
+        enableMouseTracking: false
+      }
+    },
+    series: [
+      {
+        name: 'Power consumption',
+        data: props.chartData.map(data => data.AC)
+      },
+      {
+        name: 'Power obtained',
+        data: props.chartData.map(data => data.AO)
+      },
+      {
+        name: 'Power loss',
+        data: props.chartData.map(data => data.AL)
+      }
+    ]
+  };
+
+  const optionsBarChart = {
+    chart: {
+      type: 'column'
+    },
+    title: {
+      text: props.titleChart
+    },
+    subtitle: {
+      text: 'Source: RDIPs.com'
+    },
+    xAxis: {
+      categories: hourData,
+      title: {
+        text: `Time (${DateFilter[props.timeType]})`
+      },
+      crosshair: true
+    },
+    yAxis: {
+      min: 0,
+      title: {
+        text: `A (${props.chartData[0].unit})`
+      }
+    },
+    tooltip: {
+      headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
+      pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
+        '<td style="padding:0"><b>{point.y:.1f} mm</b></td></tr>',
+      footerFormat: '</table>',
+      shared: true,
+      useHTML: true
+    },
+    plotOptions: {
+      column: {
+        pointPadding: 0.2,
+        borderWidth: 0
+      }
+    },
+    series: [
+      {
+        name: 'Power consumption',
+        data: props.chartData.map(data => data.AC)
+      },
+      {
+        name: 'Power obtained',
+        data: props.chartData.map(data => data.AO)
+      },
+      {
+        name: 'Power loss',
+        data: props.chartData.map(data => data.AL)
+      }
+    ]
+  };
+
+
+  function highChartsRender(ops: any) {
+    Highcharts.chart({
+      chart: {
+        type: 'pie',
+        renderTo: 'abc',
+      },
+      title: {
+        verticalAlign: 'middle',
+        floating: true,
+        text: "Earth's Atmospheric Composition",
+        style: {
+          fontSize: '10px',
+        },
+      },
+      plotOptions: {
+        pie: {
+          dataLabels: {
+            format: '{point.name}: {point.percentage:.1f} %',
+          },
+          innerSize: '70%',
+        },
+      },
+      series: ops,
+    });
+  }
+
+  useEffect(()=>{
+
+  },[])
+
+  return (
+    <div className='highchart-container'>
+      <HighchartsReact
+        highcharts={Highcharts}
+        options={getOptions(props.typeChart)}
+      />
+    </div>
+  );
+};
