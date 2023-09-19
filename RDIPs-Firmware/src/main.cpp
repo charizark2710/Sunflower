4cbabba0bd3f3f18764d9ad589e98616a2d820e2
thanh85tttt <thanh85ttttt@gmail.com>
2023-09-19 21:24:39 +0700

    [RDIPs-BE][SFR-237] Handle post device api

--- RDIPs-Firmware/src/main.cpp
+++ RDIPs-Firmware/src/main.cpp
@@ -1,86 +1,167 @@
 #include <Arduino.h>
 #include <WiFi.h>
 #include <PubSubClient.h>
-#include <HttpClient.h>
+#include <Arduino_JSON.h>
+#include <UUID.h>
 
 #define RELAY 12
 #define LED 2
 
-const char *ssid = "Ngo Van Nhieu";
-const char *password = "01698592252";
+const char *ssid = "Trung Thanh";
+const char *password = "thanh0106";
 
 // MQTT Broker
-const char *mqtt_server = "192.168.1.183"; // 192.168.1.183 //172.24.64.1
-const char *topic = "devices.device1.PUT/device";
-const char *mqtt_username = "admin"; // admin
-const char *mqtt_password = "admin"; // admin
-const int mqtt_port = 1883;          // 1883
+const char *mqtt_server = "192.168.1.3"; // 192.168.1.183 //172.24.64.1
+const char *mqtt_username = "admin";     // admin
+const char *mqtt_password = "admin";     // admin
+const int mqtt_port = 1883;              // 1883
 
 const unsigned long interval = 10000; // 10 seconds (in milliseconds)
 unsigned long previousMillis = 0;
+unsigned long currentMillis;
 
-// HttpRequest
-const char *uri = "http://192.168.1.183:8080/";
-const char *content_type_header = "Content-Type";
-const char *content_type_value = "application/json";
+// Topic to receive messages from server
+const String exchangeReceived = "server.*";
+
+// Topic to send messages
+const String deviceName = "device_name";
+const String exchangeSend = "gateway.";
+const String postDevice = ".PostDevice";
 
 WiFiClient espClient;
 PubSubClient client(espClient);
 
-void setupWifi()
+UUID uuid;
+
+String correlationId;
+String receiveMessage;
+String deviceId;
+
+void generateCorrelationId()
 {
-  delay(100);
-  Serial.print("\nConnecting to");
-  Serial.print(ssid);
-
-  WiFi.begin(ssid, password);
-  // this setting for public wifi (no-password)
-  // WiFi.begin(ssid);
-  WiFi.mode(WIFI_STA);
-
-  while (WiFi.status() != WL_CONNECTED)
-  {
-    Serial.print(".");
-    delay(100);
-  }
-
-  Serial.print("\nConnected to");
-  Serial.println(ssid);
+  uuid.setVariant4Mode();
+  uuid.generate();
+  correlationId = uuid.toCharArray();
 }
 
+String getSendTopic(String topic)
+{
+  return exchangeSend + deviceName + topic;
+}
+
+String getReceiveTopic()
+{
+  return exchangeReceived;
+}
+
+void postDeviceApi()
+{
+  // Create request body
+  JSONVar data;
+  JSONVar body;
+  body["name"] = WiFi.macAddress();
+  body["type"] = "family";
+  body["status"] = "Active";
+  body["firmware_ver"] = 1;
+  body["app_ver"] = 1;
+
+  generateCorrelationId();
+  data["CorrelationId"] = uuid.toCharArray();
+  data["body"] = body;
+
+  // Convert JSON object to string
+  String request = JSON.stringify(data);
+
+  // Publish devices.device1.POST/device
+  Serial.println("POST/device: " + request);
+  bool isPublished = client.publish(getSendTopic(postDevice).c_str(), request.c_str());
+  if (isPublished)
+  {
+    String receiveTopic = getReceiveTopic();
+    client.subscribe(receiveTopic.c_str());
+    Serial.print("Subscribe topic: ");
+    Serial.println(receiveTopic);
+  }
+}
+
+void handlePostDeviceResponse(char *topic, String receiveMessage)
+{
+  String receiveTopic = "server/+";
+  bool isTopicMatched = (String)topic == receiveTopic;
+
+  if (!isTopicMatched)
+  {
+    return;
+  }
+
+  Serial.print("Handle message with topic: ");
+  Serial.println(topic);
+
+  JSONVar response = JSON.parse(receiveMessage);
+  String correlationIdResponse = response["CorrelationId"];
+  if (correlationIdResponse == correlationId)
+  {
+    int httpCode = response["httpCode"];
+    String message = response["message"];
+    if (httpCode == 200)
+    {
+      JSONVar data = response["data"];
+      String id = data["Id"];
+      deviceId = id;
+      Serial.print("Post device successed with id: ");
+      Serial.println(deviceId);
+    } 
+    else if (message != "")
+    {
+      Serial.print("Post device fail, with error ");
+      Serial.print(httpCode);
+      Serial.print(", cause is ");
+      Serial.println(message);
+    }
+  }
+}
+
+// Callback function for received messages
 void callback(char *topic, byte *payload, unsigned int length)
 {
+  Serial.println("-----------------------");
   Serial.print("Message arrived in topic: ");
   Serial.println(topic);
   Serial.print("Message:");
+  receiveMessage = "";
   for (int i = 0; i < length; i++)
   {
+    receiveMessage.concat((char)payload[i]);
     Serial.print((char)payload[i]);
   }
   Serial.println();
-  Serial.println("-----------------------");
+  handlePostDeviceResponse(topic, receiveMessage);
 }
 
-String setupPutDeviceApi()
+// Connect to WiFi network
+void connectWiFi()
 {
-  // Make HTTP POST request
-  HTTPClient http;
-  http.begin(uri);
-  http.addHeader(content_type_header, content_type_value);
-  int httpResponseCode = http.PUT("{\"name\": \"Device 33\",\"type\": \"Family\"}");
-  String response = http.getString();
-  http.end();
-  return response.c_str();
+  Serial.print("Connecting to WiFi...");
+
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println();
+  Serial.print("WiFi connected. IP address: ");
+  Serial.println(WiFi.localIP());
 }
 
-void setup()
+// Connect to MQTT broker
+void connectMQTT()
 {
-  delay(3000);
-  Serial.begin(115200);
-  setupWifi();
-
   client.setServer(mqtt_server, mqtt_port);
+  client.setBufferSize(1000);
   client.setCallback(callback);
+
   while (!client.connected())
   {
     String client_id = "esp32-client-";
@@ -88,41 +169,38 @@ void setup()
     Serial.printf("The client %s connects to the public MQTT broker\n", client_id.c_str());
     if (client.connect(client_id.c_str(), mqtt_username, mqtt_password))
     {
-      Serial.println("Connected to RabbitMQ");
+      Serial.println("Connected to MQTT broker.");
     }
     else
     {
       Serial.print("Connection failed, rc=");
       Serial.println(client.state());
-      delay(2000);
+      delay(5000);
     }
   }
-  // Publish and subscribe
-  client.subscribe(topic);
-  client.publish(topic, "Hi, I'm ESP32 ^^");
+}
+
+void setup()
+{
+  Serial.begin(115200);
+  connectWiFi();
+  connectMQTT();
+
+  // Call postDeviceApi
+  postDeviceApi();
 }
 
 void loop()
 {
-  unsigned long currentMillis = millis();
+  currentMillis = millis();
   if (currentMillis - previousMillis > interval)
   {
     if (WiFi.status() == WL_CONNECTED)
     {
       previousMillis = currentMillis;
-
-      // String response = setupPutDeviceApi();
-      HTTPClient http;
-      http.begin(uri);
-      http.addHeader(content_type_header, content_type_value);
-      int httpResponseCode = http.PUT("{\"name\": \"Device 33\",\"type\": \"Family\"}");
-      String response = http.getString();
-      http.end();
-      // Publish the API response to an MQTT topic
-      // client.publish(topic, response.c_str());
-      // if(httpResponseCode > 0)
-      client.publish(topic, http.getString().c_str() );
     }
   }
   client.loop();
+  delay(5000);
 }
+
