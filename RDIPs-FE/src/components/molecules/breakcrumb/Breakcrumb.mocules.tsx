9038106b7c3fabfb40cca88dc0db5ac924f4fc85
Truc Ly <truclyy38@gmail.com>
2023-12-11 23:15:35 +0700

    add search bar and update breakcumb ui

--- RDIPs-FE/src/components/molecules/breakcrumb/Breakcrumb.mocules.tsx
+++ RDIPs-FE/src/components/molecules/breakcrumb/Breakcrumb.mocules.tsx
@@ -4,25 +4,33 @@ import { ButtonAtom } from '../../atoms/button/Button.atom';
 import { SimpleDialog } from '../../atoms/dialog/Dialog.atom';
 import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router';
+import { SearchAtom } from '../../atoms/search/Search.atom';
 
 interface BreakCrumbProps {
   title: string;
   modal?: React.ReactNode;
   icon?: React.ReactNode;
   status?: string;
-  level?: 1|2|3|4|5|6|7;
+  level?: 1 | 2 | 3 | 4 | 5 | 6 | 7;
   link?: string;
 }
 
-const BreakCrumbMocule: React.FC<BreakCrumbProps> = ({ title, icon = <></>, modal, status, level = 2, link = "/admin" }) => {
+const BreakCrumbMocule: React.FC<BreakCrumbProps> = ({
+  title,
+  icon = <></>,
+  modal,
+  status,
+  level = 2,
+  link = '/admin',
+}) => {
   const navigate = useNavigate();
   let addTitle = `Add ${title}`;
-  let path = level === 2?  "Home" : "Home/...";
+  let path = level === 2 ? 'Home' : 'Home/...';
 
   const [open, setOpen] = useState(false);
 
   useEffect(() => {
-    if(status === "closed") {
+    if (status === 'closed') {
       handleClose();
     }
   }, [status]);
@@ -35,26 +43,43 @@ const BreakCrumbMocule: React.FC<BreakCrumbProps> = ({ title, icon = <></>, moda
     setOpen(false);
   };
 
-  const redirectToHome= () => {
+  const redirectToHome = () => {
     navigate('/admin');
-  }
+  };
 
-  const redirectToLink= () => {
-    navigate(link, {replace: true});
-  }
+  const redirectToLink = () => {
+    navigate(link, { replace: true });
+  };
 
   return (
     <div className='breakcrumb-container'>
-      <SimpleDialog title={addTitle} children={modal} open={open} onClose={handleClose} />
-      <div className='breakcrumb-small'>
-        <span className='underline' onClick={redirectToHome}>{path}</span>{'/'}
-        <span className='underline' onClick={redirectToLink}>
-          {icon} {title}
-        </span>
-      </div>
-      <div className='breakcrumb-big flex-justify-space-between'>
-        {title}
-        <ButtonAtom children={'Add New'} buttonStyle='add-button' onClick={handleClickOpen} />
+      <SimpleDialog
+        title={addTitle}
+        children={modal}
+        open={open}
+        onClose={handleClose}
+      />
+      <div className='flex-justify-space-between'>
+        <div>
+          <div className='breakcrumb-small'>
+            <span className='underline' onClick={redirectToHome}>
+              {path}
+            </span>
+            {'/'}
+            <span className='underline' onClick={redirectToLink}>
+              {icon} {title}
+            </span>
+          </div>
+          <div className='breakcrumb-big'>{title}</div>
+        </div>
+        <div className='flex-align-center w-max'>
+          <SearchAtom />
+          <ButtonAtom
+            children={'Add New'}
+            buttonStyle='add-button'
+            onClick={handleClickOpen}
+          />
+        </div>
       </div>
     </div>
   );
