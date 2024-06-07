b788a63b494f6e3ef8cc393324f1d211753863af
Anh Phan <pbt.anh19992@gmail.com>
2024-06-07 18:02:32 +0700

    Smoothing motor operation

--- RDIPs-Firmware/Controller/src/main.cpp
+++ RDIPs-Firmware/Controller/src/main.cpp
@@ -11,6 +11,11 @@ void rotate(int destinationDegrees);
 
 void setup()
 {
+
+  pinMode(ENA, OUTPUT);
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
+
   Serial.begin(9600); // Initiate serial communication for printing the results on the Serial monitor
   Wire.begin();       // Initiate the Wire library
   // Set ADXL345 in measuring mode
@@ -29,6 +34,8 @@ void loop()
   {
     exit(0);
   }
+  // gyro.getAngle();
+  // gyro.displayAngleByModel(0);
 }
 
 bool checkDevice()
@@ -49,8 +56,14 @@ bool checkDevice()
     Serial.println("Gyro functional");
   }
   Serial.println("Checking on motor module");
+  rotate(45);
+  delay(1000);
   rotate(30);
+  delay(1000);
   rotate(-30);
+  delay(1000);
+  rotate(-45);
+  delay(1000);
   rotate(0);
   Serial.println("Motor functional");
 
@@ -60,28 +73,91 @@ bool checkDevice()
 //==The more forward the more Degree increase from negative to positive==//
 void rotate(int destinationDegrees)
 {
+
   Serial.println("Rolling");
 
+  // Get current degrees
   gyro.getAngle();
   int currentDegrees = (int)gyro.X_degree;
-  Serial.print("currentDegrees = ");
-  Serial.println(currentDegrees);
+  // Serial.print("currentDegrees = ");
+  // Serial.println(currentDegrees);
 
-  while (abs(destinationDegrees - currentDegrees) != 0)
+  int distant = abs(destinationDegrees - currentDegrees); // get absolute distant
+  int speed[2];                                           // speed[0] is minimum speed, speed[1] is maximum speed
+  int duration = 0;                                       // the duration of the motor while it running
+  // int times = 0;
+
+  // The farther the distance is, the faster and longer the motor run
+  while (distant != 0)
   {
+
+    distant = abs(destinationDegrees - currentDegrees); // Get distant again to calculate speed and duration
+    // Serial.print("Distant: ");
+    // Serial.println(distant);
+
+    if (distant > 30)
+    {
+      speed[0] = SPEED_2;
+      speed[1] = SPEED_3;
+      duration = 1000;
+    }
+
+    else if (distant > 10 && distant <= 20)
+    {
+      speed[0] = SPEED_1;
+      speed[1] = SPEED_2;
+      duration = 200;
+    }
+
+    else if (distant > 4 && distant <= 10)
+    {
+      speed[0] = SPEED_1;
+      speed[1] = SPEED_2;
+      duration = 170;
+    }
+
+    else if (distant > 1 && distant <= 4)
+    {
+      speed[0] = SPEED_1;
+      speed[1] = SPEED_2;
+      duration = 100;
+    }
+
+    else if (distant == 1)
+    {
+      speed[0] = SPEED_1;
+      speed[1] = SPEED_1;
+      duration = 160;
+    }
+
+    else if (distant == 0)
+    {
+      motor.stop();
+      Serial.println("Finish rotation");
+      // Serial.print("Time: ");
+      // Serial.println(times);
+      break;
+    }
+
+    // If destinationn degrees is greater than current degrees, the motor will rotate forward
     if (destinationDegrees > currentDegrees)
     {
-      motor.forward(MAX_SPEED);
-      delay(abs(destinationDegrees - currentDegrees) * 100);
+      motor.accelerate(speed[0], speed[1], 0);
+      delay(duration);
       motor.stop();
-    }
-    else
-    {
-      motor.backward(MAX_SPEED);
-      delay(abs(destinationDegrees - currentDegrees) * 100);
-      motor.stop();
+      // times += 1;
     }
 
+    // If destinationn degrees is smaller than current degrees, the motor will rotate backward
+    else if (destinationDegrees < currentDegrees)
+    {
+      motor.accelerate(speed[0], speed[1], 1);
+      delay(duration);
+      motor.stop();
+      // times += 1;
+    }
+
+    // get current degrees again to re-calculate speed and duration
     gyro.getAngle();
     currentDegrees = (int)gyro.X_degree;
     gyro.displayAngleByModel(0);
--- RDIPs-Firmware/Controller/src/motor.h
+++ RDIPs-Firmware/Controller/src/motor.h
@@ -4,10 +4,12 @@
 
 #include "gyro.h"
 
+#define ENA 9
 #define IN1 7
 #define IN2 8
-#define MAX_SPEED 255
-#define MIN_SPEED 0
+#define SPEED_3 255
+#define SPEED_2 170
+#define SPEED_1 85
 
 typedef struct Motor
 {
@@ -17,21 +19,68 @@ typedef struct Motor
         digitalWrite(IN1, LOW);
         digitalWrite(IN2, LOW);
     }
+
     //==Rotate counter-clockwise==//
+    // One speed only
     void forward(int speed)
     {
         Serial.println("Forwarding");
-        speed = constrain(speed, MIN_SPEED, MAX_SPEED);
+        analogWrite(ENA, speed);
         digitalWrite(IN1, HIGH);
-        analogWrite(IN2, 255 - speed);
+        digitalWrite(IN2, LOW);
     }
+
     //==Rotate clockwise==//
+    // One speed only
     void backward(int speed)
     {
         Serial.println("Backwarding");
-        speed = constrain(speed, MIN_SPEED, MAX_SPEED);
+        analogWrite(ENA, speed);
         digitalWrite(IN1, LOW);
-        analogWrite(IN2, speed);
+        digitalWrite(IN2, HIGH);
+    }
+
+    // Smoothing motor speed by increase it's speed from minimum speed to max speed
+    // If direction == 0, the motor rotate forward
+    // If direction == 1, the motor rotate backward
+    void accelerate(int minSpeed, int maxSpeed, int direction)
+    {
+        if (minSpeed != maxSpeed)
+        {
+            for (int i = minSpeed; i <= maxSpeed; i++)
+            {
+                analogWrite(ENA, i);
+            }
+            switch (direction)
+            {
+            case 0:
+                Serial.println("forwarding");
+                digitalWrite(IN1, HIGH);
+                digitalWrite(IN2, LOW);
+                break;
+            case 1:
+                Serial.println("backwarding");
+                digitalWrite(IN1, LOW);
+                digitalWrite(IN2, HIGH);
+                break;
+            default:
+                break;
+            }
+        }
+        else
+        {
+            switch (direction)
+            {
+            case 0:
+                forward(minSpeed);
+                break;
+            case 1:
+                backward(minSpeed);
+                break;
+            default:
+                break;
+            }
+        }
     }
 
 } Motor;
