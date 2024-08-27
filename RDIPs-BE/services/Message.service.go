d0d23c58dc70cd3d74c7f28b60fb3eeab9e5cac2
Dang Duc Hieu <dangduchieudn99@gmail.com>
2024-08-27 21:39:05 +0700

    [Common] update filter for performance and other things

--- RDIPs-BE/services/Message.service.go
+++ RDIPs-BE/services/Message.service.go
@@ -20,12 +20,13 @@ var TransmitMessage = func(c *commonModel.ServiceContext) (commonModel.ResponseT
 	var payloadMSG interface{}
 	err := json.Unmarshal(c.Body, &payloadMSG)
 	if err == nil {
-		err := handler.NewDeviceHandler(c.Ctx, &model.SysDevices{}).ReadDetail(false, deviceID)
+		var device model.SysDevices
+		err := handler.NewDeviceHandler(c.Ctx, &device).ReadDetail(false, deviceID)
 		if err != nil {
 			return commonModel.ResponseTemplate{HttpCode: 500, Data: nil, Message: err.Error()}, err
 		}
 		messageHandler := handler.NewMessageHandler()
-		messageHandler.Send("amq."+amqp091.ExchangeTopic, payloadMSG, uuid.NewString(), deviceID)
+		messageHandler.Send("amq."+amqp091.ExchangeTopic, payloadMSG, uuid.NewString(), device.Name)
 		return commonModel.ResponseTemplate{HttpCode: 200, Data: "Transmit successfully"}, nil
 	}
 	return commonModel.ResponseTemplate{HttpCode: 500, Data: nil}, err
